51c94b54dc06df48b3f4126c71875d672d312d43
ShriramDensity <shriram@density.exchange>
2023-10-27 23:17:01 +0530

    finished basic functionality of guessing songs

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react';
 import useAuth from '../customHooks/useAuth';
 import SpotifyWebApi from 'spotify-web-api-node';
 import Player from './Player';
+import { songObject } from './songObject';
 
 const spotifyApi = new SpotifyWebApi({
 	clientId: 'c111d8cd377f4cf0946e51bf5470f005',
@@ -11,6 +12,10 @@ const spotifyApi = new SpotifyWebApi({
 const Dashboard = ({ code }) => {
 	const accessToken = useAuth(code);
 	const [guessingSongs, setGuessingSongs] = useState([]);
+	const [currentSong, setCurrentSong] = useState(songObject);
+	const [userGuess, setUserGuess] = useState();
+	const [songNumber, setSongNumber] = useState(0);
+	const [showAnswerTitle, setShowAnswerTitle] = useState(false);
 
 	useEffect(() => {
 		if (!accessToken) return;
@@ -20,7 +25,7 @@ const Dashboard = ({ code }) => {
 	useEffect(() => {
 		if (!accessToken) return;
 
-		spotifyApi.searchPlaylists('best of 2000s').then(
+		spotifyApi.searchPlaylists('top 2010s english').then(
 			function (data) {
 				console.log('Found playlists are', data.body);
 
@@ -40,11 +45,50 @@ const Dashboard = ({ code }) => {
 		);
 	}, [accessToken]);
 
-	console.log('hello there', guessingSongs);
+	useEffect(() => {
+		if (!guessingSongs[0]) return;
+		setCurrentSong(guessingSongs[0]);
+	}, [guessingSongs]);
+
+	const captureGuess = () => {
+		if (userGuess.toUpperCase().trim() === currentSong.track.name.toUpperCase().split('(')[0].trim()) {
+			setUserGuess('');
+			setSongNumber(songNumber + 1);
+			setCurrentSong(guessingSongs[songNumber]);
+			setShowAnswerTitle(false);
+			alert('Correct!');
+		} else {
+			alert('Wrong!');
+		}
+	};
+
+	const goNextSong = () => {
+		setUserGuess('');
+		setSongNumber(songNumber + 1);
+		setCurrentSong(guessingSongs[songNumber]);
+		setShowAnswerTitle(false);
+	};
 
 	return (
 		<div>
-			<Player accessToken={accessToken} trackUri="afdsf" />
+			<Player accessToken={accessToken} trackUri={currentSong.track.uri} />
+			<input
+				value={userGuess}
+				onChange={(e) => {
+					setUserGuess(e.target.value);
+				}}
+			></input>
+			<button onClick={() => captureGuess()}>Guess!!</button>
+			<button onClick={() => goNextSong()}>goNext</button>
+			<button
+				onClick={() => {
+					setShowAnswerTitle(true);
+				}}
+			>
+				showAnswer
+			</button>
+
+			{showAnswerTitle && <h1>{currentSong.track.name}</h1>}
 		</div>
 	);
 };
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,9 +1,25 @@
 /* eslint-disable react/prop-types */
 import SpotifyPlayer from 'react-spotify-web-playback';
+import './PlayerStyles.css';
 
 const Player = ({ accessToken, trackUri }) => {
-	// if (!accessToken) return null;
-	return <SpotifyPlayer token={accessToken} showSaveIcon uris={trackUri ? [trackUri] : []} />;
+	if (!accessToken) return null;
+	return (
+		<SpotifyPlayer
+			token={accessToken}
+			showSaveIcon
+			uris={trackUri ? [trackUri] : []}
+			styles={{
+				bgColor: '#333',
+				color: '#fff',
+				loaderColor: '#fff',
+				sliderColor: '#1cb954',
+				savedColor: '#fff',
+				trackArtistColor: '#333',
+				trackNameColor: '#333',
+			}}
+		/>
+	);
 };
 
 export default Player;
